96f0da7daea3ad7bf1d8ef94ae290a9a4ee57c32
Eng Cabdishakur MH <miighostt@gmail.com>
2026-08-03 16:09:51 +0300

    Remove duplicate Order button from ControlButtons so only single primary Order button displays on ActionPad

--- ss_print_pos_kitchen_receipt/__manifest__.py
+++ ss_print_pos_kitchen_receipt/__manifest__.py
@@ -12,7 +12,7 @@
     'author': "Smartron Solutions Pvt Ltd",
     'website': "https://smartronsolutions.co.uk",
     'category': 'Point of Sale',
-    'version': '19.0.1.0.4',
+    'version': '19.0.1.0.5',
     'data': [
             'views/views.xml',
         ],
