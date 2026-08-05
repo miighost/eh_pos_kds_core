7b04a7eaddd8d8c12258dbdaf08641d3fba8546f
Eng Cabdishakur MH <miighostt@gmail.com>
2026-08-05 17:25:33 +0300

    m

--- eh_pos_kds_restaurant/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import eh_kds_ticket
-from . import eh_kds_ticket_item
-from . import eh_kds_card
-from . import pos_order
--- eh_pos_kds_restaurant/models/eh_kds_ticket_item.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class EhKdsTicketItem(models.Model):
-    _inherit = "eh.kds.ticket.item"
-
-    eh_course_id = fields.Many2one("restaurant.order.course", string="Course", ondelete="set null")
-    eh_course_index = fields.Integer(related="eh_course_id.index", store=True)
